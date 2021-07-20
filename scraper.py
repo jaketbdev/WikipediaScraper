ca313a2d214dfb32abd4492fdf8f873643e934f3
jaketbdev <72199858+jaketbdev@users.noreply.github.com>
2021-07-20 12:02:12 +0100

    Extracting a random link to another wikipediea article

--- scraper.py
+++ scraper.py
@@ -1,9 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import random
 
 response = requests.get(
     url="https://en.wikipedia.org/wiki/Web_scraping")
 
+# Initalise soup from the response and exctract the article title
 soup = BeautifulSoup(response.content, 'html.parser')
 title = soup.find(id="firstHeading")
 print(title.string)
+
+# Getting all the links from the article
+
+allLinks = soup.find(id="bodyContent").find_all("a")
+random.shuffle(allLinks)
+linkToScraper = 0
+
+for link in allLinks:
+    if link['href'].find("/wiki/") == -1:
+        continue
+    linkToScrape = link
+    break
+
+print(linkToScrape)
