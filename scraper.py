c27e17121107845a5a44e23d75973be7bee04bec
jaketbdev <72199858+jaketbdev@users.noreply.github.com>
2021-07-20 12:08:09 +0100

    Formatted the code into a class, and made it an endless scraper

--- scraper.py
+++ scraper.py
@@ -2,24 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import random
 
-response = requests.get(
-    url="https://en.wikipedia.org/wiki/Web_scraping")
+def scrapeWikiArticle(url):
+    response = requests.get(
+        url=url
+        )
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-# Initalise soup from the response and exctract the article title
-soup = BeautifulSoup(response.content, 'html.parser')
-title = soup.find(id="firstHeading")
-print(title.string)
+    title = soup.find(id="firstHeading")
+    print(title.text)
 
-# Getting all the links from the article
+    allLinks = soup.find(id="bodyContent").find_all("a")
+    random.shuffle(allLinks)
+    linkToScraper = 0
 
-allLinks = soup.find(id="bodyContent").find_all("a")
-random.shuffle(allLinks)
-linkToScraper = 0
+    for link in allLinks:
+        if link['href'].find('/wiki/') == -1:
+            continue
 
-for link in allLinks:
-    if link['href'].find("/wiki/") == -1:
-        continue
-    linkToScrape = link
-    break
+        linkToScrape = link
+        break
 
-print(linkToScrape)
+    scrapeWikiArticle("https://en.wikipedia.org" + linkToScrape['href'])
+
+scrapeWikiArticle("https://en.wikipedia.org/wiki/Web_scraping")
